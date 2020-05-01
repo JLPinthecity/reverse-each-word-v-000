76ab4457a64e08e494d8a889c2e735cf82d4313b
Joann Pan <joannlpan@gmail.com>
2020-05-01 19:43:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word(sentence)
-  words = sentence.split(" ")
-  words.collect do |word|
+  sentence.split.collect do |word|
     word.reverse
   end
+  reverse_each_word(sentence).join(" ")
+  
 
 end
 
